6769b644654bf957d89f7bd3d2209ca02287fe80
Topher Hughes <hughestopher86@mail.com>
2017-12-02 13:33:46 -0800

    added main menu

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour {
+
+	public Transform Spawn;
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			GameObject player = other.transform.parent.gameObject;
+			player.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			player.transform.position = Spawn.position;
+		} else {
+			Destroy (other.gameObject);
+		}
+	}
+
+}
---
+++ Assets/Scripts/MenuCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamera : MonoBehaviour {
+
+	public float TransitionTime = 0.5f;
+
+	private bool Transitioning = false;
+	private Vector3 StartPos;
+	private Vector3 EndPos;
+	private float StartTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Transitioning) {
+			float currentTime = (Time.time - StartTime) / TransitionTime;
+			transform.position = Vector3.Lerp (StartPos, EndPos, currentTime);
+			if (currentTime > 1f) {
+				Transitioning = false;
+			}
+		}
+	}
+
+	public void StartTransition(Vector3 newPos){
+		//print ("starting a transition");
+		StartPos = transform.position;
+		EndPos = newPos;
+		StartTime = Time.time;
+		Transitioning = true;
+	}
+}
