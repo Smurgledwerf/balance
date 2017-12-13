91945a21b045e1904d7a9b022f1ab5d396b4e3ff
Veena Vignale <veenavignale@Veenas-MacBook-Pro.local>
2017-12-12 21:52:31 -0800

    rope swinging iteration 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,51 +4,101 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	
-	//private CharacterController controller; 
 	private Rigidbody rb; 
 	private float speed = 13f;
-	private float verticalVelocity; 
+	private Vector3 jumpVector; 
 	private float gravity = 14f; 
 	private float jumpForce = 100f;
 	private float turnSpeed = 4f; 
+	private bool onRope = false; 
+	private GameObject rope; 
+
 	// Use this for initialization
 	void Start () {
 		rb = gameObject.GetComponent<Rigidbody>();
+		jumpVector = Vector3.zero;
 	}
 
 	// Check if the player is on the ground by casting a ray directly downward
-	// and checking if it hits a collider within a tolerance value 
+	// and checking if it hits a collider within a tolerance 
 	bool grounded(float tolerance){
 		return Physics.Raycast (transform.position, new Vector3 (0, -1, 0), tolerance);	// tolerance should be roughly the length of the character
 	}
 
-	
+	public void setOnRope(GameObject r){
+		onRope = true; 	
+		rope = r; 
+	}
+
+	public bool getOnRope(){
+		return onRope;
+	}
+		
 	// Update is called once per frame
 	void Update () {
-		
-		if(grounded(1.5f)){
-			verticalVelocity = -gravity * Time.deltaTime; 
 
-			if (Input.GetKeyDown(KeyCode.Space)){
-				verticalVelocity = jumpForce; 
+		// TODO make it do the goddamn thing it's fucking supposed to
+		// also stop using so many magic numbers or at least make them macros
+		if (grounded (1.5f)) {	
+
+			jumpVector.y = -gravity * Time.deltaTime;
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				jumpVector.y = jumpForce; 
+			}
+		} else if (onRope) {
+			/*
+			Vector3 negative = (Vector3.zero - jumpVector).normalized; 
+
+			jumpVector.x += negative.x * 100 * Time.deltaTime; 
+			jumpVector.z += negative.z * 100 * Time.deltaTime;
+
+			//jumpVector.x += (0 - jumpVector.x) * gravity * Time.deltaTime;
+			*/
+			rb.Sleep (); 
+			if (Input.GetKeyDown (KeyCode.Space)) {
+
+				// jump vector perpendicular to the player's current rotation 
+				//var cross = Vector3.Cross (transform.right, transform.localEulerAngles.normalized);
+				//jumpVector = cross * 12; 
+
+				// using rope velocity  
+				rb.WakeUp();
+				Vector3 jump = rope.GetComponent<Rigidbody> ().velocity * 5; 
+				jump.y = 5; 
+				jumpVector = jump; 
+				transform.localEulerAngles = Vector3.zero;
+				rb.velocity = Vector3.zero;
+				onRope = false;
 			}
 		}
-		else{
-			verticalVelocity -= gravity * Time.deltaTime; 
-		}
-			
-		// move in 3d based upon input 
-		var x = Input.GetAxisRaw("P1Horizontal") * Time.deltaTime;
-		var z = Input.GetAxisRaw("P1Vertical") * Time.deltaTime;
 
-		Vector3 motion = new Vector3 (x, 0, z)*speed;
+		// jump dampening force 
+		Vector3 negative = (Vector3.zero - jumpVector);
+		float deltaX = negative.x * 10 * Time.smoothDeltaTime;
+		float deltaZ = negative.z * 10 * Time.smoothDeltaTime;
+		Vector3 newVel = new Vector3 (rb.velocity.x + deltaX, rb.velocity.y, rb.velocity.z + deltaZ);
+
+		jumpVector.x += deltaX;
+		jumpVector.z += deltaZ;
+		jumpVector.y -= gravity * Time.deltaTime; 
+
+		// move in 3d based upon input 
+		var x = Input.GetAxis ("P1Horizontal") * Time.smoothDeltaTime;
+		var z = Input.GetAxis ("P1Vertical") * Time.smoothDeltaTime;
+
+		Vector3 motion = new Vector3 (x, 0, z) * speed;
 
 		// orient player along direction of motion 
 		if (motion != Vector3.zero)
-			transform.rotation = Quaternion.Lerp (transform.rotation,  Quaternion.LookRotation(-motion), Time.deltaTime*turnSpeed);
+			transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation (-motion), Time.deltaTime * turnSpeed);
 
 		transform.Translate (motion * speed * Time.deltaTime, Space.World);
-		rb.AddForce (new Vector3 (0, verticalVelocity, 0), ForceMode.Impulse);
+
+		if (onRope){	// TODO distinguish a "swingable" rope vs pendulum 
+			rope.GetComponent<Rigidbody>().AddForce (motion * speed * Time.deltaTime, ForceMode.VelocityChange);
+		}
+			
+		rb.AddForce (jumpVector, ForceMode.Impulse);
 
 	}
 }
--- Assets/Scripts/RopeController.cs
+++ Assets/Scripts/RopeController.cs
@@ -4,69 +4,34 @@ using UnityEngine;
 
 public class RopeController : MonoBehaviour {
 
-	private Vector3 prevPos; 
-	Quaternion prevRot; 
-	bool playerOn;
 	GameObject player; 
 
 	// Use this for initialization
 	void Start () {
-		prevPos = gameObject.transform.position;
-		prevRot = gameObject.transform.rotation;
-		playerOn = false; 
 		player = null; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		if (playerOn && player.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl> ().getOnRope ()){
-
-			// get calculate position delta and apply
-			Vector3 currPos = gameObject.transform.position; 
-			Vector3 delta = currPos - prevPos; 
-			//print (delta); 
-
-			//apply delta to character 
-			// player.transform.position += delta; 
-
-
-			Vector3 localRot = gameObject.transform.localEulerAngles.normalized;
+		if (player && player.GetComponent<PlayerController> ().getOnRope ()){
 
 			// apply rotation 
 			player.transform.rotation = gameObject.transform.rotation; 
-			player.transform.position = gameObject.transform.position - .7f*gameObject.transform.forward.normalized;
+			player.transform.position = gameObject.transform.position - .5f*gameObject.transform.up.normalized;
 			
 		}
-		else{
-			playerOn = false; 
-		}
-		prevPos = gameObject.transform.position;
-	}
-
-	public void setPlayerOn(bool p){
-		playerOn = p;
 	}
 
 	void OnTriggerEnter(Collider other){
 		if(other.gameObject.tag == "Player"){
-			playerOn = true; 
-			print ("player on");
 			player = other.gameObject;
-			player.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl> ().setOnRope (true);
+			if (!player.GetComponent<PlayerController> ().getOnRope ()) {
+				player.GetComponent<PlayerController> ().setOnRope (gameObject);
+			}
+
 		}
 	}
-
-	void OnTriggerExit(Collider other){
-		if (other.gameObject.tag == "Player"){
-			//playerOn = false; 
-			//print ("player off");
-			//player = null; 
-			//other.gameObject.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl> ().setOnRope (false);
-		}
-	}
-
-
 
 
 }
